d4ffd5f92f095815a75ddfd873a6a4b495e46403
xhly93 <1725837808@qq.com>
2020-04-10 17:56:48 +0800

    Add files via upload

---
+++ unit_test.py
@@ -0,0 +1,45 @@
+import unittest
+
+import pytest
+
+
+class Test_py(unittest.TestCase):
+    def setUp(self):
+        print('setUp')
+        pass
+
+    def tearDown(self):
+        print('tearDown')
+        pass
+
+
+
+    def test_01(self):
+        print('001')
+        pass
+
+    def test_02(self):
+        print('002')
+        aa
+        pass
+
+
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+        pass
+
+
+if __name__ == '__main__':
+    # pytest.main(["s", "-q", "--alluredir", "./report"])
+    # pytest.main('--alluredir = C:/Users/Administrator/.jenkins/workspace/test/allure-result')
+    suit = unittest.TestSuite()
+    suit.addTest(Test_py('test_01'))
+    suit.addTest(Test_py('test_02'))
+    runner = unittest.TextTestRunner()
+    runner.run(suit)
